61ab0aa143d0285c5f5d42d8ac585e20030b1225
Mohammad Saiful Islam <saifmdislam231@gmail.com>
2023-05-23 04:49:49 -0700

    basic navbar, footer and banner section added

--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -1,10 +1,14 @@
 import { Outlet, ScrollRestoration } from "react-router-dom";
+import Footer from "../pages/Shared/Footer/Footer";
+import Navbar from "../pages/Shared/Navbar/Navbar";
 
 const Main = () => {
     return (
-        <div>
+        <div className="relative">
+            <Navbar/>
             <ScrollRestoration/>
             <Outlet/>
+            <Footer/>
         </div>
     );
 };
