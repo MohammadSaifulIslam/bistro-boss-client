7d6a33c63e1cfbd26a8b223ef184a87a0ead4936
Mohammad Saiful Islam <saifmdislam231@gmail.com>
2023-05-29 01:43:55 -0700

    all user show in a taboler form and make user admin function added

--- src/pages/AllUsers/AllUsers.jsx
+++ src/pages/AllUsers/AllUsers.jsx
@@ -1,4 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
+import { FaTrash, FaUserShield } from "react-icons/fa";
+import Swal from "sweetalert2";
 
 const AllUsers = () => {
     const { data: users = [], refetch } = useQuery(['users'], async () => {
@@ -6,10 +8,59 @@ const AllUsers = () => {
         return res.json();
 
     })
+    const handleAdmin = (user) => {
+        fetch(`http://localhost:5000/users/admin/${user._id}`, {
+            method: "PATCH"
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.modifiedCount) {
+                    Swal.fire({
+                        icon: 'success',
+                        title: `${user.name} is an admin now.`,
+                        text: 'You successfully make an admin'
+                    })
+                    refetch()
+                }
+            })
+    }
     return (
-        <div>
-            <p>All Users {users.length}</p>
-        </div>
+        <section className="md:w-11/12 mx-auto h-full py-20">
+            <h4 className=" text-3xl font-bold mb-5">Total Users {users.length}</h4>
+            <div>
+                <div className="overflow-x-auto">
+                    <table className="table w-full">
+                        {/* head */}
+                        <thead className="text-white">
+                            <tr >
+                                <th className="bg-accent">#</th>
+                                <th className="bg-accent">Name</th>
+                                <th className="bg-accent">Email</th>
+                                <th className="bg-accent">Role</th>
+                                <th className="bg-accent">Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                users.map((user, index) => <tr
+                                    key={user._id}
+                                >
+                                    <th>{index + 1}</th>
+                                    <td>{user.name}</td>
+                                    <td>{user.email}</td>
+                                    {
+                                        user.role === 'admin' ? <td>Admin</td>
+                                            : <td><button onClick={() => handleAdmin(user)} className="my-btn"><FaUserShield /></button></td>
+                                    }
+                                    <td><button className="btn btn-error text-white"><FaTrash /></button></td>
+                                </tr>)
+                            }
+
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </section>
     );
 };
 
